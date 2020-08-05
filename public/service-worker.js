409ddaf873ba91d82accfd93a2954e3a29d1397c
Myhyun <mattyihyun@gmail.com>
2020-08-05 15:34:31 -0700

    added db.js file for offline to online storage

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,6 @@
-const CACHE_NAME = "static-cache-v2"
-
-const DATA_CACHE_NAME = "data-cache-v1";
-
 const FILES_TO_CACHE = [
     '/',
+    '/db.js',
     '/index.html',
     '/index.js',
     '/manifest.webmanifest',
@@ -12,19 +9,19 @@ const FILES_TO_CACHE = [
     '/assets/images/icons/icon-512x512.png',
 ];
 
+const CACHE_NAME = "static-cache-v2"
+
+const DATA_CACHE_NAME = "data-cache-v1";
+
+
+
 self.addEventListener("install", function (evt) {
-    // pre cache image data
     evt.waitUntil(
         caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
     );
-
-    // pre cache all static assets
     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
     );
-
-    // tell the browser to activate this service worker immediately once it
-    // has finished installing
     self.skipWaiting();
 });
 
@@ -46,26 +43,21 @@ self.addEventListener("activate", function (evt) {
 });
 
 self.addEventListener('fetch', function (evt) {
-    // code to handle requests goes here
     if (evt.request.url.includes("/api/")) {
         evt.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(evt.request)
                     .then(response => {
-                        // If the response was good, clone it and store it in the cache.
                         if (response.status === 200) {
                             cache.put(evt.request.url, response.clone());
                         }
-
                         return response;
                     })
                     .catch(err => {
-                        // Network request failed, try to get it from the cache.
                         return cache.match(evt.request);
                     });
             }).catch(err => console.log(err))
         );
-
         return;
     }
 
